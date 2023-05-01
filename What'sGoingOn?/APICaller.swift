e27be04c763f65f0748653e328c0e0dd40f640e9
Connie Hyman <connie2630a@gmail.com>
2023-04-30 21:53:31 -0700

    Completed api call

---
+++ What'sGoingOn?/APICaller.swift
@@ -0,0 +1,8 @@
+//
+//  APICaller.swift
+//  What'sGoingOn?
+//
+//  Created by Constance Hyman on 4/30/23.
+//
+
+import Foundation
---
+++ What'sGoingOn?/NewsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  NewsTableViewCell.swift
+//  What'sGoingOn?
+//
+//  Created by Constance Hyman on 4/30/23.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
